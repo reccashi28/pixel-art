042a102815b971afcb66253aaa9622ff148dd137
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-08-23 10:50:22 +0200

    add dynamic hover effect

--- src/components/DrawingPanel/DrawingPanel.tsx
+++ src/components/DrawingPanel/DrawingPanel.tsx
@@ -17,10 +17,6 @@ const useStyles = makeStyles( {
   panelCell: {
       width: 20,
       height: 20,
-      '&:hover':{
-          backgroundColor: 'gray',
-          curso: 'default'
-      },
       borderWidth: 1,
       borderColor: 'black',
       borderStyle: 'solid',
@@ -35,14 +31,29 @@ function DrawingPanel() {
     const classes = useStyles();
     const { dimension } = useSelector( (state: AppState) => state.drawingPanel)
     const [color, setColor ] = useState<colorChangePropType>({hex: '#000'})
+    const [canChangeCollor, setCanChangeCollor] = useState<boolean>(true)
+    const [oldColor, setOldColor] = useState<string>('')
 
     const changeBackgroundColor = (e: React.MouseEvent<HTMLTableHeaderCellElement, MouseEvent>): void => {
         e.currentTarget.style.backgroundColor = `${color.hex}`
+        setCanChangeCollor(false)
+    }
+
+    const changeBackgroundColorOnHover = (e: React.MouseEvent<HTMLTableHeaderCellElement, MouseEvent>):void => {
+        setOldColor(e.currentTarget.style.backgroundColor)
+        e.currentTarget.style.backgroundColor = `${color.hex}`;
+    }
+
+    const resetBackgroundColor = (e: React.MouseEvent<HTMLTableHeaderCellElement, MouseEvent>):void => {
+        if(canChangeCollor) {
+            e.currentTarget.style.backgroundColor = `${oldColor}`;
+        }
+        setCanChangeCollor(true)
     }
      const createColumnDrawingPanel = () => {
         let cell = []
         for(let i = 0; i < dimension; i++) {
-            cell.push(<TableCell className={classes.panelCell} key={i} onClick={changeBackgroundColor}></TableCell>)
+            cell.push(<TableCell className={classes.panelCell} key={i}  onMouseEnter={changeBackgroundColorOnHover} onMouseLeave={resetBackgroundColor} onClick={changeBackgroundColor}></TableCell>)
         }
 
         return cell;
